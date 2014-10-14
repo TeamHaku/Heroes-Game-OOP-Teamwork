34cff7d880cfaedd7819068a3b864eb04fd53b01
bsdemon <eon@abv.bg>
2014-10-14 15:18:44 +0300

    InfernoCreatures and HeavenUnits added

--- Game/Game/Units/HeavenUnits.cs
+++ Game/Game/Units/HeavenUnits.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Game.WarUnits
+﻿namespace Game.Units
 {
-    using Game.Units;
-
     public static class HeavenUnits
     {
         private static Unit sentinel;
+        private static Unit crossbowman;
+        private static Unit sister;
+        private static Unit griffin;
+        private static Unit celestial;
 
         public static Unit Sentinel
         {
@@ -30,10 +26,77 @@ namespace Game.WarUnits
             }
         }
 
+        public static Unit Crossbowman
+        {
+            get
+            {
+                return crossbowman;
+            }
 
-        //Crossbowman,
-        //Sister,
-        //Griffin,
-        //Celestial
+            private set
+            {
+                crossbowman = new Unit(
+                   attackPoints: new PointsRange(2, 5),
+                   defence: new PointsRange(3, 7),
+                   healthPoints: 20,
+                   squareMovements: 5,
+                   cost: new Cost(110));
+            }
+        }
+
+        public static Unit Sister
+        {
+            get
+            {
+                return sister;
+            }
+
+            private set
+            {
+                sister = new Unit(
+                   attackPoints: new PointsRange(5, 7),
+                   defence: new PointsRange(1, 2),
+                   healthPoints: 17,
+                   squareMovements: 5,
+                   cost: new Cost(105));
+            }
+        }
+
+        public static Unit Griffin
+        {
+            get
+            {
+                return griffin;
+            }
+
+            private set
+            {
+                griffin = new Unit(
+                   attackPoints: new PointsRange(14, 18),
+                   defence: new PointsRange(12, 25),
+                   healthPoints: 80,
+                   squareMovements: 6,
+                   cost: new Cost(490));
+            }
+        }
+
+        public static Unit Celestial
+        {
+            get
+            {
+                return celestial;
+            }
+
+            private set
+            {
+                celestial = new Unit(
+                   attackPoints: new PointsRange(54, 56),
+                   defence: new PointsRange(40, 57),
+                   healthPoints: 325,
+                   squareMovements: 6,
+                   cost: new Cost(2190));
+            }
+        }
+
     }
 }
