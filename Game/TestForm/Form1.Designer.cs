1bac07f58fca62a5a11825cb16f75033687df1d5
bankin <niky.bankin@gmail.com>
2014-10-15 20:30:56 +0300

    Map genereted from map.txt; hero moving on empty spaces

--- Game/TestForm/Form1.Designer.cs
+++ Game/TestForm/Form1.Designer.cs
@@ -32,22 +32,77 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxType = new System.Windows.Forms.TextBox();
+            this.gameLog = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(572, 12);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 20);
+            this.textBoxX.TabIndex = 0;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(572, 38);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 20);
+            this.textBoxY.TabIndex = 1;
+            // 
+            // textBoxType
+            // 
+            this.textBoxType.Location = new System.Drawing.Point(572, 64);
+            this.textBoxType.Name = "textBoxType";
+            this.textBoxType.Size = new System.Drawing.Size(100, 20);
+            this.textBoxType.TabIndex = 2;
+            // 
+            // gameLog
+            // 
+            this.gameLog.Location = new System.Drawing.Point(572, 103);
+            this.gameLog.Name = "gameLog";
+            this.gameLog.Size = new System.Drawing.Size(100, 346);
+            this.gameLog.TabIndex = 3;
+            this.gameLog.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(569, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Game Log:";
             // 
             // GameWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(379, 329);
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gameLog);
+            this.Controls.Add(this.textBoxType);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "GameWindow";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private TextBox textBoxX;
+        private TextBox textBoxY;
+        private TextBox textBoxType;
+        private RichTextBox gameLog;
+        private Label label1;
 
 
 
--- Game/TestForm/Form1.cs
+++ Game/TestForm/Form1.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿#define DEBUG
+//#undef DEBUG
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,22 +20,23 @@ namespace TestForm
     using Game.Units;
 
     using TestForm.Properties;
-
+    
     public partial class GameWindow : Form
     {
         public GameWindow()
         {
             this.InitializeComponent();
-            this.DrawBattlefiled();
+            //this.DrawBattlefiled();
             this.InitializeGame();
+            this.DrawMap();
         }
 
         private void InitializeGame()
         {
             LivePlayer player = new LivePlayer(
-                    Faction.Heaven,
-                    Hero.DeffaultHero,
-                    HeavenUnits.DeffaultHeavenUnits);
+                Faction.Heaven,
+                Hero.DeffaultHero,
+                HeavenUnits.DeffaultHeavenUnits);
         }
 
         public void DrawBattlefiled()
@@ -43,7 +48,7 @@ namespace TestForm
                     Button butt = new Button();
                     butt.BackColor = Color.White;
                     butt.BackgroundImageLayout = ImageLayout.Stretch;
-                    butt.Location = new Point(col * 50, row * 50);
+                    butt.Location = new Point(col*50, row*50);
                     butt.Name = string.Format("{0}-{1}", row, col);
                     butt.Size = new Size(50, 50);
                     this.Controls.Add(butt);
@@ -59,46 +64,151 @@ namespace TestForm
 
         public void DrawMap()
         {
-            Map map = new Map(new MapCell[,]
-            {
-                {
-                    new MapCell(0, 0, Resources.enemy), 
-                    new MapCell(0, 1, Resources.enemy),
-                    new MapCell(0, 3, Resources.enemy),
-                    new MapCell(0, 4, Resources.enemy),
-                    new MapCell(0, 5, Resources.enemy),
-                    new MapCell(0, 6, Resources.enemy),
-                    new MapCell(0, 7, Resources.enemy),
-                    new MapCell(0, 8, Resources.enemy),
-                    new MapCell(0, 9, Resources.enemy),
-                    new MapCell(0, 10, Resources.enemy),
-                    new MapCell(0, 11, Resources.enemy),
-                    new MapCell(0, 12, Resources.enemy)
-                },
-
-                {
-                    new MapCell(1, 0, Resources.enemy), 
-                    new MapCell(1, 1, Resources.enemy),
-                    new MapCell(1, 3, Resources.enemy),
-                    new MapCell(1, 4, Resources.enemy),
-                    new MapCell(1, 5, Resources.enemy),
-                    new MapCell(1, 6, Resources.enemy),
-                    new MapCell(1, 7, Resources.enemy),
-                    new MapCell(1, 8, Resources.enemy),
-                    new MapCell(1, 9, Resources.enemy),
-                    new MapCell(1, 10, Resources.enemy),
-                    new MapCell(1, 11, Resources.enemy),
-                    new MapCell(1, 12, Resources.enemy)
-                }
-            });
-
-            //for (int row = 0; row < map.MapCells[0].; row++)
+            //Map map = new Map(new MapCell[,]
             //{
-            //    for (int i = 0; i < UPPER; i++)
             //    {
-                    
+            //        new MapCell(0, 0, Resources.enemy), 
+            //        new MapCell(0, 1, Resources.enemy),
+            //        new MapCell(0, 3, Resources.enemy),
+            //        new MapCell(0, 4, Resources.enemy),
+            //        new MapCell(0, 5, Resources.enemy),
+            //        new MapCell(0, 6, Resources.enemy),
+            //        new MapCell(0, 7, Resources.enemy),
+            //        new MapCell(0, 8, Resources.enemy),
+            //        new MapCell(0, 9, Resources.enemy),
+            //        new MapCell(0, 10, Resources.enemy),
+            //        new MapCell(0, 11, Resources.enemy),
+            //        new MapCell(0, 12, Resources.enemy)
+            //    },
+
+            //    {
+            //        new MapCell(1, 0, Resources.enemy), 
+            //        new MapCell(1, 1, Resources.enemy),
+            //        new MapCell(1, 3, Resources.enemy),
+            //        new MapCell(1, 4, Resources.enemy),
+            //        new MapCell(1, 5, Resources.enemy),
+            //        new MapCell(1, 6, Resources.enemy),
+            //        new MapCell(1, 7, Resources.enemy),
+            //        new MapCell(1, 8, Resources.enemy),
+            //        new MapCell(1, 9, Resources.enemy),
+            //        new MapCell(1, 10, Resources.enemy),
+            //        new MapCell(1, 11, Resources.enemy),
+            //        new MapCell(1, 12, Resources.enemy)
             //    }
-            //}
+            //});
+            try
+            {
+                using (StreamReader sr = new StreamReader("../../../map.txt"))
+                {
+                    for (int row = 0; row < 8; row++)
+                    {
+                        String line = sr.ReadLine();
+                        for (int col = 0; col < 10; col++)
+                        {
+                            MapCell cell = new MapCell(row, col, Resources.earth);
+                            string name = "not recognized";
+                            switch (line[col])
+                            {
+                                case 'W':
+                                    cell.Image = Resources.tree;
+                                    name = "tree";
+                                    break;
+                                case 'S':
+                                    cell.Image = Resources.stone;
+                                    name = "stone";
+                                    break;
+                                case 'C':
+                                    cell.Image = Resources.Sister;
+                                    name = "castle";
+                                    break;
+                                case 'H':
+                                    cell.Image = Resources.player;
+                                    name = "player";
+                                    break;
+                                case 'X':
+                                    cell.Image = Resources.enemy;
+                                    name = "enemy";
+                                    break;
+                                case 'E':
+                                    cell.Image = Resources.earth;
+                                    name = "earth";
+                                    break;
+                                case 'T':
+                                    cell.Image = Resources.chest;
+                                    name = "chest"; //tresure
+                                    break;
+                            }
+
+                            this.Controls.Add(generateButton(cell, name));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                this.textBoxType.Text = "The file could not be read:";
+                this.gameLog.Text = e.Message;
+            }
+
+        }
+
+        private Button generateButton(MapCell cell, string name)
+        {
+            Button butt = new Button();
+            butt.BackColor = Color.White;
+            butt.Image = cell.Image;
+            butt.BackgroundImageLayout = ImageLayout.Stretch;
+            butt.Location = new Point(cell.Y*50, cell.X*50);
+            butt.Name = name; //string.Format("{0}-{1}", cell.Y, cell.X);
+            butt.Size = new Size(50, 50);
+            butt.Click += new EventHandler(this.button_Clicked);
+            return butt;
+        }
+
+        private void button_Clicked(object sender, EventArgs e)
+        {
+            Button triggeredButton = (Button) sender;
+            int x = triggeredButton.Location.X/50;
+            int y = triggeredButton.Location.Y/50;
+            string type = triggeredButton.Name;
+            string log = "nothing";
+
+     
+#if (DEBUG)
+            this.textBoxX.Text = x + " " + this.textBoxX.Text;
+            this.textBoxY.Text = y + " " + this.textBoxY.Text;
+            this.textBoxType.Text = type;
+#endif
+
+            if (type == "earth") //Cell is empty
+            {
+                Button player = (Button) this.Controls.Find("player", false)[0];
+                this.Controls.Remove(triggeredButton);
+                this.Controls.Remove(player);
+                Point playerLocation = player.Location;
+                player.Location = triggeredButton.Location;
+                triggeredButton.Location = playerLocation;
+                this.Controls.Add(player);
+                this.Controls.Add(triggeredButton);
+                log = "Walk";
+            }
+            else if (type == "tree") //Or other element you cannot walk on
+            {
+                log = "Can't walk here, sorry";
+            }
+            else if (type == "enemy") //Or other enemy
+            {
+                log = "fight";
+            }
+            else if (type == "castle")
+            {
+                log = "enter your castle";
+            }
+            else if (type == "chest")
+            {
+                log = "collect tresure if path available";
+            }
+            this.gameLog.Text = log + Environment.NewLine + this.gameLog.Text;
 
         }
 
